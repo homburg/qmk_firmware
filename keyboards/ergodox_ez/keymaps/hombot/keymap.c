a09f008a40645cff90254710783a178569787728
Thomas B Homburg <thomas@madethis.dk>
2022-08-03 09:38:50 +0200

    ...

--- keyboards/ergodox_ez/keymaps/hombot/keymap.c
+++ keyboards/ergodox_ez/keymaps/hombot/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    MO(SYMB),             MO(SYMB),     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_LBRC,
   KC_RCTRL,        KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,              KC_SCLN,        GUI_T(KC_QUOT),
   KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    KC_RGUI,              MEH_T(KC_NO),          KC_N,    KC_M,    KC_COMM, KC_DOT,   CTL_T(KC_SLSH), KC_RSFT,
-  MO(MDIA),    KC_QUOT,     KC_RALT, KC_RALT, KC_RGUI,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
+  MO(MDIA),    KC_QUOT,     KC_RALT, KC_RALT, KC_RGUI,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, KC_BSLS,
                                                            ALT_T(KC_APP), KC_LGUI,                KC_LALT, CTL_T(KC_ESC),
                                                                           KC_HOME,                 KC_PGUP,
                                                          KC_LGUI, KC_BSPC, KC_END,                  KC_PGDN, KC_ENT, KC_SPC
