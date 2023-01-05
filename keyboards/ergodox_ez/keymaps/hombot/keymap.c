fe5c4b2c8d103866c11254db9634ab5fa6b1ef1d
Thomas B Homburg <thomas@madethis.dk>
2023-01-05 11:50:06 +0100

    Lots of stuff
    - Adjust number row
    - Adjust left alt/</>

--- keyboards/ergodox_ez/keymaps/hombot/keymap.c
+++ keyboards/ergodox_ez/keymaps/hombot/keymap.c
@@ -18,18 +18,21 @@ enum custom_keycodes {
     MY_CIRC,
 };
 
+// NB! Tips!!!!!
+// KC_TRNS = Transparent to the layer below, ie. use the keymap of the layer below
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_GRV,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_6,         KC_7,    KC_8,    KC_9,    KC_0,              KC_MINS,        KC_EQL,
+  KC_GRV,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_EQL,       KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,        
   KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    MO(SYMB),             MO(SYMB),     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_LBRC,
   KC_RCTRL,        KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,              KC_SCLN,        GUI_T(KC_QUOT),
   KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    KC_RGUI,              MEH_T(KC_NO),          KC_N,    KC_M,    KC_COMM, KC_DOT,   CTL_T(KC_SLSH), KC_RSFT,
-  MO(MDIA),    KC_QUOT,     KC_RALT, KC_RALT, KC_RGUI,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, KC_BSLS,
+  MO(MDIA),        KC_NUBS,     KC_RALT, KC_RALT, KC_RGUI,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, KC_BSLS,
                                                            ALT_T(KC_APP), KC_LGUI,                KC_LALT, CTL_T(KC_ESC),
                                                                           KC_HOME,                 KC_PGUP,
-                                                         KC_LGUI, KC_BSPC, KC_END,                  KC_PGDN, KC_ENT, KC_SPC
+                                                         KC_SPC, KC_BSPC, KC_END,                  KC_PGDN, KC_ENT, KC_SPC
 ),
 /* Keymap 1: Symbol Layer
  *
@@ -58,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_EXLM,       KC_HASH,    LSA(KC_8),   LSA(KC_9),   KC_PIPE, KC_TRNS,     KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
   KC_TRNS, RALT(KC_NUHS), KC_GRV,     LSFT(KC_8),  LSFT(KC_9),  LSFT(KC_4),               KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
   KC_TRNS, LSFT(KC_5),    MY_CIRC,    RALT(KC_8),  RALT(KC_9),  LSFT(KC_GRV), KC_TRNS,     KC_TRNS, KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-  EEP_RST, KC_TRNS, KC_TRNS, KC_NUBS, LSFT(KC_NUBS),                                         KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+  EEP_RST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
                                                RGB_MOD, KC_TRNS,     RGB_TOG, RGB_M_P,
                                                         KC_TRNS,     KC_TRNS,
                                       RGB_VAD, RGB_VAI, KC_TRNS,     KC_TRNS, RGB_HUD, RGB_HUI
@@ -90,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_MPLY,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,                                         KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_RALT, KC_RALT, KC_LGUI,                                         KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
 
                                                KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
                                                         KC_TRNS,     KC_TRNS,
